246c90430c822aa5309d8b8345fc8050a613ee20
JBONU <janardhan.bonu@monsanto.com>
2016-05-01 13:15:08 -0500

    test

--- src/chapter6/ParallelJacobi.c
+++ src/chapter6/ParallelJacobi.c
@@ -1,7 +1,7 @@
 /* Parallel Jacobi Relaxation program in C* language */
 
 #define n 32 /* Size of the array*/
-#define numiter 20 /*Number of iterations*/
+#define numiter 1 /*Number of iterations*/
 float A[n+2][n+2], B[n+2][n+2];
 int i,j,k;
 
@@ -14,14 +14,22 @@ main() {
     }
 
     B = A;
-
-    for(k=0;k<= numiter;k++) {
-        for(i=0;i<=n;i++) {
-            for(j=0;j<=n;j++) {
-                B[i][j] = (A[i-1][j] + A[i+1][j]+A[i][j-1] + A[i][j+1])/4;
-                A = B;
-            }
+    /* Perform only one iteration */
+    for(k=1;k<= numiter;k++) {
+        /* 1. In the 2X2 matrix create a new process for each row */
+        forall i=1 to n do {
+          int j;
+          for(j=1;j<=n;j++) {
+            B[i][j] = (A[i-1][j] + A[i+1][j] + A[i][j-1] + A[i][j+1])/4;
+          }
         }
+
+        /*
+         * 2. Copy new values back to A row by row.
+         * Why it should be row by row instead of A = B ???
+         */
+        forall i=1 to n do
+          A[i] = B[i];
     }
 
 }
