31e3a97905a0fdebc5f951e4365a3d907673d3e1
JBONU <janardhan.bonu@monsanto.com>
2016-05-01 12:03:19 -0500

    -

---
+++ src/chapter6/SerialJacobi.c
@@ -0,0 +1,22 @@
+/* Sequential Jacobi Relaxation program in C* language */
+
+#define n 32 /* Size of the array*/
+#define numiter 20 /*Number of iterations*/
+float A[n+2][n+2], B[n+2][n+2];
+int i,j,k;
+
+main() {
+    /* Initialize array values */
+    for(i=0;i <= n+1;i++) {
+        for(j=0;j<=n+1;j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    B = A;
+
+    for()
+
+    /**/
+
+}
